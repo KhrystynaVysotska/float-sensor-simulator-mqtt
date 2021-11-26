caafe5941e62f3ad4f5c6043c1c8cd2693953834
Khrystyna <khrystynavysotska@gmail.com>
2021-11-26 15:37:29 +0200

    Changed expected path to iot core certificates

--- configs/gcp_configs.py
+++ configs/gcp_configs.py
@@ -4,7 +4,7 @@ REGION = os.environ.get('REGION')
 PROJECT_ID = os.environ.get('PROJECT_ID')
 REGISTRY_ID = os.environ.get('REGISTRY_ID')
 
-CERTIFICATE_FILE_PATH = "./keys/roots.pem"
+CERTIFICATE_FILE_PATH = "./certs/iot-core-certs.pem"
 PRIVATE_KEY_FILE_PATH = "./keys/rsa_private.pem"
 
 DEFAULT_TOPIC = "events"
